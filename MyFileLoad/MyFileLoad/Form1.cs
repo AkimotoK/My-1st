e6654b2640534fd64663a2781909f24a4c349b51
AkimotoK <s3fcceb8e7v4tyw@yahoo.co.jp>
2015-10-13 16:20:59 +0900

    メッセージ

--- MyFileLoad/MyFileLoad/Form1.cs
+++ MyFileLoad/MyFileLoad/Form1.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+ *Form1.cs 
+ * 学籍番号　1323163 秋本謙史
+ * 
+ * Kinect演習第1回～第2回の演習のメインのソースコード
+ * 課題1:ファイル入出力
+ * 課題2:加算機を作る
+ * 課題3:タイマーを使い現在時刻を表示する
+ * 
+ * 最終更新日 2015/10/13
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +29,9 @@ namespace MyFileLoad
         {
             InitializeComponent();
         }
+
+        int DelayMsgCounter = 0;
+        int Total { get; set; }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -54,6 +70,23 @@ namespace MyFileLoad
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var input = int.Parse(textBox2.Text);
+            Total += input;
+            label3.Text = Total.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
 
         }
